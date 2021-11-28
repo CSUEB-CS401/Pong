49aa7f2f01e2c89969e555a35c89e77b1f4d37b2
Jonathan <jzepeda19@horizon.csueastbay.edu>
2021-11-27 23:34:21 -0800

    11/27 Main Menu Implemented. 🔧

---
+++ src/main/java/edu/csueastbay/cs401/jzepeda/myMainMenuControls.java
@@ -0,0 +1,33 @@
+package edu.csueastbay.cs401.jzepeda;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import java.io.IOException;
+import java.util.Objects;
+
+public class myMainMenuControls {
+
+    @FXML
+    private BorderPane baseBorderPane;
+
+    @FXML
+    ImageView imageLogo;
+
+    @FXML
+    ImageView imageView;
+
+    @FXML
+    void goBack(ActionEvent event) {
+        try {
+            Parent root;
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/edu/csueastbay/cs401/jzepeda/login.fxml")));
+            baseBorderPane.setCenter(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
