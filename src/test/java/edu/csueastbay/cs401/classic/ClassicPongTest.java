a2a640b194a2ed5ba7537d9871f6ab013d5051ce
Paul Raupach <paul@raupach.com>
2021-11-30 12:42:40 -0800

    Fixing an error in my HittingPlayer2GoalAddAPointToPlayer2 test.

--- src/test/java/edu/csueastbay/cs401/classic/ClassicPongTest.java
+++ src/test/java/edu/csueastbay/cs401/classic/ClassicPongTest.java
@@ -187,7 +187,7 @@ class ClassicPongTest {
                 110);
 
         game.collisionHandler(puck, bang);
-        assertEquals(2, game.getPlayerScore(1));
+        assertEquals(1, game.getPlayerScore(2));
     }
 
 
