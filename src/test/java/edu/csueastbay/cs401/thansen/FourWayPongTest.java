9ce12a40636051802e84bf14be86a9084bb7d563
taylorhansen <taylor@hansenfamily.us>
2021-11-29 20:11:57 -0800

    Add more tests

--- src/test/java/edu/csueastbay/cs401/thansen/FourWayPongTest.java
+++ src/test/java/edu/csueastbay/cs401/thansen/FourWayPongTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FourWayPongTest {
 
@@ -17,7 +17,7 @@ public class FourWayPongTest {
 
     @BeforeEach
     void setUp() {
-        game = new FourWayPong(10, 1300, 860);
+        game = new FourWayPong(10, 1300, 1300);
     }
 
     @Test
@@ -188,5 +188,91 @@ public class FourWayPongTest {
 
         game.collisionHandler(puck, bang);
         assertEquals(1, game.getPlayerScore(4));
+    }
+
+    @Test
+    void shouldHaveNoDeadPlayersAtStart() {
+        for (int player = 1; player < FourWayPong.NUM_PLAYERS; ++player) {
+            shouldBeAlive(player);
+        }
+    }
+
+    @Test
+    void shouldHaveNoVictorAtStart() {
+        shouldHaveNoVictor();
+    }
+
+    @Test
+    void shouldEliminatePlayerAfterAccumulatingPoints() {
+        game.addPointsToPlayer(1, game.getLoseScore());
+        shouldBeDead(1);
+    }
+
+    @Test
+    void shouldStillHaveNoVictorAfterPlayerEliminated() {
+        game.addPointsToPlayer(1, game.getLoseScore());
+        shouldBeDead(1);
+        shouldHaveNoVictor();
+    }
+
+    @Test
+    void shouldHaveVictorAfterEliminatingAllButOnePlayer() {
+        game.addPointsToPlayer(1, game.getLoseScore());
+        shouldBeDead(1);
+        shouldHaveNoVictor();
+        game.addPointsToPlayer(3, game.getLoseScore());
+        shouldBeDead(3);
+        shouldHaveNoVictor();
+        game.addPointsToPlayer(4, game.getLoseScore());
+        shouldBeDead(4);
+        shouldHaveVictor(2);
+    }
+
+    private void shouldBeAlive(int player) {
+        assertEquals(
+                0, game.getPlayerScore(player),
+                "Player " + player + " should have a score of 0."
+        );
+        assertEquals(
+                0, game.playerScoreProperty(player).get(),
+                "Player " + player + " should have a score of 0 via property."
+        );
+        assertTrue(game.isPlayerAlive(player), "Player " + player + " should still be alive.");
+        assertTrue(game.playerAlive(player).get(), "Player " + player + " should still be alive via binding.");
+        assertFalse(game.isPlayerDead(player), "Player " + player + " should not be dead.");
+        assertFalse(game.playerDead(player).get(), "Player " + player + " should not be dead via binding.");
+    }
+
+    private void shouldBeDead(int player) {
+        assertTrue(
+                game.getPlayerScore(player) >= game.getLoseScore(),
+                "Player " + player + " should have more than " + game.getLoseScore() + " points."
+        );
+        assertTrue(
+                game.playerScoreProperty(player).get() >= game.getLoseScore(),
+                "Player " + player + " should have more than " + game.getLoseScore() + " points via property."
+        );
+        assertFalse(game.isPlayerAlive(player), "Player " + player + " should not be alive.");
+        assertFalse(game.playerAlive(player).get(), "Player " + player + " should not be alive via binding.");
+        assertTrue(game.isPlayerDead(player), "Player " + player + " should be dead.");
+        assertTrue(game.playerDead(player).get(), "Player " + player + " should be dead via binding.");
+    }
+
+    private void shouldHaveNoVictor() {
+        assertEquals(0, game.getVictor(), "Should have no victor");
+        assertEquals(0, game.victorProperty().get(), "Should have no victor via binding");
+        assertFalse(game.isGameOver(), "Should not be game over");
+        assertFalse(game.gameOver().get(), "Should not be game over via binding");
+        assertTrue(game.isStillPlaying(), "Should still be playing");
+        assertTrue(game.stillPlaying().get(), "Should still be playing via binding");
+    }
+
+    private void shouldHaveVictor(int player) {
+        assertEquals(player, game.getVictor(), "Should have " + player + " as victor");
+        assertEquals(player, game.victorProperty().get(), "Should have " + player + " as victor via binding");
+        assertTrue(game.isGameOver(), "Should be game over");
+        assertTrue(game.gameOver().get(), "Should be game over via binding");
+        assertFalse(game.isStillPlaying(), "Should not be still playing");
+        assertFalse(game.stillPlaying().get(), "Should not be still playing via binding");
     }
 }
